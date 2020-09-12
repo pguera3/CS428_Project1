44f8b06838db9c878b9c7f5666c68b3b8330bf3d
pguera3 <pguera3@uic.edu>
2020-09-12 15:07:05 -0500

    Alpha

---
+++ Assets/DateTeller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DateTeller : MonoBehaviour
+{
+    public GameObject dateTextObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateDate", 0f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dateTextObject.GetComponent<TextMeshPro>().text = System.DateTime.Now.ToString("M/dd/yy");
+    }
+}
